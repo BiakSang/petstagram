b7a757333510d40035a32b0fc90cd880a41d1826
BiakSang <munsongbiaksang@gmail.com>
2020-02-05 21:59:12 +0530

    v1 splashscreen updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,21 @@
-const hideSplashscreen = () => {
-    setTimeout(() => {
-        document.getElementById("splashscreen").setAttribute("class", "hidden");
-    }, 500);
+const controlSplashscreen = () => {
+    if (Storage !== undefined && sessionStorage.getItem("splashscreen") !== "loaded"){
+        document.getElementById("splashscreen").setAttribute("class", "visible");
+        
+        setTimeout(() => {
+            document.getElementById("splashscreen").setAttribute("class", "");
+            document.getElementById("app").setAttribute("class", "visible");
+            sessionStorage.setItem("splashscreen", "loaded");
+        }, 500);
+    }
+    else {
+        document.getElementById("app").setAttribute("class", "visible");
+    }
 }
 const initialFunction = () => {
     updateData();
     setPetType();
-    hideSplashscreen();
+    controlSplashscreen();
 }
 
 window.onload = initialFunction;
